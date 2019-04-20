fe84284472ca0e52bef8b326797e265115b989b1
Lpsilva1 <leonardopereira519@gmail.com>
2019-04-20 14:26:58 -0300

    Inclusão de componentes basicos de interface

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import QRCode from "react-native-qrcode-svg";
 
 class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      textCode: ""
+      textCode: "",
+      qrCodeValue: ""
     };
   }
 
@@ -20,21 +22,45 @@ class HomeScreen extends Component {
 
   handleGenerateButton = () => {
     const { textCode } = this.state;
-    console.warn(textCode);
+    this.setState({ qrCodeValue: textCode });
   };
 
   render = () => {
-    const { textCode } = this.state;
+    const { textCode, qrCodeValue } = this.state;
     return (
-      <View>
-        <TextInput
-          value={textCode}
-          onChangeText={this.onChangeText}
-          placeholder="Texto para geração"
-        />
-        <TouchableOpacity onPress={this.handleGenerateButton}>
-          <Text>Gerar</Text>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <View style={{ marginHorizontal: 10, marginVertical: 10 }}>
+          <TextInput
+            value={textCode}
+            onChangeText={this.onChangeText}
+            placeholder="Conteudo"
+          />
+        </View>
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 10,
+            backgroundColor: "#CCC",
+            padding: 10,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 5
+          }}
+          disabled={textCode === ""}
+          onPress={this.handleGenerateButton}
+        >
+          <Text style={{ alignSelf: "center" }}>Gerar</Text>
         </TouchableOpacity>
+
+        {qrCodeValue !== "" && (
+          <View style={{ alignSelf: "center" }}>
+            <QRCode
+              style={{ alignSelf: "center" }}
+              value={qrCodeValue}
+              size={100}
+            />
+          </View>
+        )}
       </View>
     );
   };
